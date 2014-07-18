1588fb9e42a0b02c88d8406e2fcbe2d1fdb78cd0
truetamtam <greenclever@gmail.com>
2014-07-18 17:20:31 +0400

    Merged eupathy and prep for queue changes

--- workbench/fintech-fab/actions-calc/src/FintechFab/ActionsCalc/Queue/QueueHandler.php
+++ workbench/fintech-fab/actions-calc/src/FintechFab/ActionsCalc/Queue/QueueHandler.php
@@ -7,6 +7,26 @@ use Illuminate\Queue\Jobs\Job;
 
 class QueueHandler
 {
+	//	public function fire(Job $job, $data) {
+	//
+	//		$ch = curl_init($data['url']);
+	//		curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+	//		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	//		curl_setopt($ch, CURLOPT_POST, true);
+	//		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+	//
+	//		$response = curl_exec($ch);
+	//		$error = curl_error($ch);
+	//
+	//		if(!$response || $error) {
+	//			Log::info("Curl error: $error, response: $response");
+	//		} else {
+	//			Log::info("Curl success: $error, response: $response");
+	//			$job->delete();
+	//		}
+	//
+	//	}
+
 	public function fire(Job $job, array $data)
 	{
 		$job->delete();
